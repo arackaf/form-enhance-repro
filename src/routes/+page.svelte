47a1305449ff6aba9cb6f4a6fe69a1e2635fdd52
Adam Rackis <adamrackis@hotmail.com>
2022-11-30 22:31:16 -0500

    Bug repro

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+  import { enhance } from "$app/forms";
+
+  function enhanceFn(arg) {}
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<!-- Comment out the use:enhance={enhanceFn} to make this work -->
+<form use:enhance={enhanceFn}>
+  Type in me and hit enter <input name="search" />
+</form>
